bbea9e93f997346fce228c5ffcda1a421fb054a0
kurayoshi981 <tachp981@yahoo.co.jp>
2019-03-14 15:24:24 +0900

    Feat:投稿時間表示、tweet編集&削除のコントローラー

--- app/controllers/tweet_controller.rb
+++ app/controllers/tweet_controller.rb
@@ -1,5 +1,6 @@
 class TweetController < ApplicationController
   # before_action :authenticate_user
+  before_action :set_tweet, only: %i(edit update destroy)
 
   def index
     @tweet = Tweet.order("created_at DESC").page(params[:page]).per(6)
@@ -20,8 +21,28 @@ class TweetController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @tweet.update_attributes(tweet_params)
+      redirect_to :tweet_index
+    else
+      @tweet.valid?
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @tweet.destroy
+    redirect_to :tweet_index
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:title, :memo).merge(user: current_user)
   end
+
+  def set_tweet
+    @tweet = Tweet.find_by(id: params[:id])
 end
