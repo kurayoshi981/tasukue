5cca088a4a54dcaf9cbf5a98a04eb2428189aad6
kurayoshi981 <tachp981@yahoo.co.jp>
2019-03-25 18:33:52 +0900

    initial commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :lists, dependent: :destroy
   has_many :tweets, dependent: :destroy
-  has_one_attached :avatar
+  # has_one_attached :avatar
   has_many :likes, dependent: :destroy
   has_many :like_tweets, through: :likes, source: :tweet
 end
